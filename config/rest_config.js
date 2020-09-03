d763040023baa17782bf68829e0b681951a26111
Shubham Kumar <56603941+Shubbbu@users.noreply.github.com>
2020-09-03 22:38:40 +0530

    Add files via upload

---
+++ config/rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = "http://newsapi.org/v2/top-headlines"
+export const country_name = "in"
+export const category = "general"
+export const API_KEY = "cf2ff9be812b4c5fb0348df1f21e9c44"

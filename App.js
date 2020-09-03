9399022693d7c14497753c5ad086df8ed713c8df
Shubham Kumar <56603941+Shubbbu@users.noreply.github.com>
2020-09-03 22:35:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { AppLoading } from "expo";
+import { Container } from "native-base";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import TabsExample from "./srctab/TabsExample";
+
+import { View, StyleSheet } from "react-native";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+
+     
+        <TabsExample />
+     
+    );
+  }
+}
